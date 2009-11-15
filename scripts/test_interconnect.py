12a6254e94fea892ea71cf745af47c861cd00f33
William Mallard <wjm@llard.net>
2009-11-15 03:03:32 +0000

    Shortened fake_xaui delay to 32, since the real xaui delay is 56. Removed var_delay block (use addressable shift registers). Adjusted control script for single-cable testing.

--- scripts/test_interconnect.py
+++ scripts/test_interconnect.py
@@ -179,46 +179,50 @@ while True:
 
 	acquire(fpga)
 
+	V = read_capt(fpga, "valid", 1, read_length)
+	S = read_capt(fpga, "sync", 1, read_length)
+
 	(m0, m1, m2) = read_capt(fpga, "012", 3, read_length)
 	(m3, m4, m5) = read_capt(fpga, "345", 3, read_length)
-	(m6, m7, mZ) = read_capt(fpga, "67Z", 3, read_length)
+	(m6, m7, mz) = read_capt(fpga, "67Z", 3, read_length)
 
 	(txX, txY, txZ) = read_capt8(fpga, "clump", 3, read_length * 4)
-	(rxX, rxY, rxZ) = read_capt8(fpga, "xaui", 3, read_length * 4)
-	(xrX, xrY, xrZ) = read_capt8(fpga, "resync", 3, read_length *4)
+	# Swapped Y and Z to test with just one cable.
+	(rxX, rxZ, rxY) = read_capt8(fpga, "xaui", 3, read_length * 4)
+	(xrX, xrZ, xrY) = read_capt8(fpga, "resync", 3, read_length *4)
 
 	(tx0, tx1, tx2) = unclump(txX)
 	(tx3, tx4, tx5) = unclump(txY)
-	(tx6, tx7, txZ) = unclump(txZ)
+	(tx6, tx7, txz) = unclump(txZ)
 
 	(rx0, rx1, rx2) = unclump(rxX)
 	(rx3, rx4, rx5) = unclump(rxY)
-	(rx6, rx7, rxZ) = unclump(rxZ)
+	(rx6, rx7, rxz) = unclump(rxZ)
 
 	(xr0, xr1, xr2) = unclump(xrX)
 	(xr3, xr4, xr5) = unclump(xrY)
-	(xr6, xr7, xrZ) = unclump(xrZ)
+	(xr6, xr7, xrz) = unclump(xrZ)
 
+	# Swapped Y and Z to test with just one cable.
 	(XA, XB, XC) = read_capt(fpga, "X", 3, read_length)
-	(XA, XB, XC) = read_capt(fpga, "X", 3, read_length)
-	(YA, YB, YC) = read_capt(fpga, "Y", 3, read_length)
-	(ZA, ZB, ZC) = read_capt(fpga, "Z", 3, read_length)
+	(YA, YB, YC) = read_capt(fpga, "Z", 3, read_length)
+	(ZA, ZB, ZC) = read_capt(fpga, "Y", 3, read_length)
 
 	c_XA.set_ydata(m3)
 	c_XB.set_ydata(rx3)
-	c_XC.set_ydata(xr3)
+	c_XC.set_ydata(YA)
 	ax1.relim()
 	ax1.autoscale_view(tight=False, scalex=False, scaley=True)
 
 	c_YA.set_ydata(m4)
 	c_YB.set_ydata(rx4)
-	c_YC.set_ydata(xr4)
+	c_YC.set_ydata(YB)
 	ax2.relim()
 	ax2.autoscale_view(tight=False, scalex=False, scaley=True)
 
 	c_ZA.set_ydata(m5)
 	c_ZB.set_ydata(rx5)
-	c_ZC.set_ydata(xr5)
+	c_ZC.set_ydata(YC)
 	ax3.relim()
 	ax3.autoscale_view(tight=False, scalex=False, scaley=True)
 
