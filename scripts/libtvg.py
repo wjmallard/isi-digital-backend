9fe944e83cbd0949f9e390c00092a3a439318b62
William Mallard <wjm@llard.net>
2010-01-11 08:00:35 +0000

    Replaced lfsr code with calls to built-in rand.

--- scripts/libtvg.py
+++ scripts/libtvg.py
@@ -16,22 +16,10 @@ def sine (length, cycles=1, phase=0):
 	y = [int(A*math.sin(B*t+C)) for t in x]
 	return arrayToByteString(y)
 
-class lfsr32 (object):
-	"""A 32-stage Linear Feedback Shift Register."""
-
-	def __init__ (self, taps=[32,31,30,10], seed=0xF0E1D2C3):
-		self._taps = taps
-		self._seed = seed
-		self._state = seed
-
-	def next (self):
-		"""Generate the next state, and return a uint32."""
-		tap_shft = (x-1 for x in self._taps)
-		tap_vals = (self._state << x for x in tap_shft)
-		xor_taps = reduce(lambda x,y: x^y, tap_vals)
-		and_taps = xor_taps & 0x80000000
-		self._state = and_taps | self._state >> 1
-		return self._state
+def rand (length, seed=None):
+    random.seed(seed)
+    y = [(2**8)*random.random() for x in xrange(length)]
+    return arrayToByteString(y)
 
 def arrayToByteString (array):
 	byteStrings = [struct.pack('!1b', x) for x in array]
@@ -39,9 +27,10 @@ def arrayToByteString (array):
 	return byteString
 
 def main ():
-	l = lfsr32()
-	for i in xrange(1,32):
-		print "%.8X" % l.next()
+	s = sine(8, 1.5, .5)
+	print s
+	r = rand(8, 1234)
+	print r
 
 if __name__ == "__main__":
 	main()
