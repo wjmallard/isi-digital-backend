884e789d511eb3e68ea1b7814f3a2802442e2310
William Mallard <wjm@llard.net>
2010-01-17 06:45:48 +0000

    Gui cleanup.

--- scripts/libisigui.py
+++ scripts/libisigui.py
@@ -20,15 +20,17 @@ class IsiGui (gtk.Window):
 
 		self._canvas = None
 		self._figure = None
-		self._axes_l = None
-		self._contour_l = None
-		self._label_l = \
-			("XX_auto", "YY_auto", "ZZ_auto", \
-			 "XY_real", "YZ_real", "ZX_real", \
-			 "XY_imag", "YX_imag", "ZX_imag")
 
 		self._set_params()
+		self._set_layout()
 
+	def _set_params (self):
+		self.set_default_size(800, 600)
+		self.connect("destroy", self._quit_action)
+		self.set_title("ISI Correlator")
+		self.set_border_width(8)
+
+	def _set_layout (self):
 		vbox = gtk.VBox(False, 8)
 		self.add(vbox)
 
@@ -37,14 +39,6 @@ class IsiGui (gtk.Window):
 
 		cpanel = self._gen_cpanel()
 		vbox.pack_start(cpanel, False, False)
-
-		self.show_all()
-
-	def _set_params (self):
-		self.set_default_size(800, 600)
-		self.connect("destroy", self._quit_action)
-		self.set_title("ISI Correlator")
-		self.set_border_width(8)
 
 	def _gen_canvas (self):
 		self._figure = libisiplot.IsiFigure()
@@ -58,9 +52,9 @@ class IsiGui (gtk.Window):
 		sync_button.connect("clicked", self._sync_action)
 		cpanel.pack_start(sync_button, True, True, 0)
 
-		shell_button = gtk.Button("Shell")
-		shell_button.connect("clicked", self._shell_action)
-		cpanel.pack_start(shell_button, True, True, 0)
+		fft_shift_button = gtk.Button("FFT Shift")
+		fft_shift_button.connect("clicked", self._fft_shift_action)
+		cpanel.pack_start(fft_shift_button, True, True, 0)
 
 		quit_button = gtk.Button("Quit")
 		quit_button.connect("clicked", self._quit_action)
@@ -69,12 +63,11 @@ class IsiGui (gtk.Window):
 		return cpanel
 
 	def _sync_action (self, widget):
+		self._isi_correlator.send_sync()
 		print "Sync sent!"
 
-	def _shell_action (self, widget):
-		import IPython
-		ipshell = IPython.Shell.IPythonShellEmbed()
-		ipshell()
+	def _fft_shift_action (self, widget):
+		print "Clicked fft_shift button."
 
 	def _quit_action (self, widget):
 		gtk.main_quit()
