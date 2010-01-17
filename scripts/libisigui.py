7dffef714eb362858271f06b2f1c2135eef1048a
William Mallard <wjm@llard.net>
2010-01-17 04:41:56 +0000

    Created a simple PyGTK GUI.

---
+++ scripts/libisigui.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+
+import matplotlib
+matplotlib.use('GTKAgg')
+from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
+from matplotlib.figure import Figure
+
+class IsiGui (gtk.Window):
+	def __init__ (self):
+		gtk.Window.__init__(self)
+
+		self._canvas = None
+		self._figure = None
+
+		self._set_params()
+
+		vbox = gtk.VBox(False, 8)
+		self.add(vbox)
+
+		canvas = self._gen_canvas()
+		vbox.pack_start(canvas)
+
+		cpanel = self._gen_cpanel()
+		vbox.pack_start(cpanel, False, False)
+
+		self.show_all()
+
+	def _set_params (self):
+		self.set_default_size(800, 600)
+		self.connect("destroy", self._quit_action)
+		self.set_title("ISI Correlator")
+		self.set_border_width(8)
+
+	def _gen_canvas (self):
+		self._figure = Figure(figsize=(8,6), dpi=100)
+		self._canvas = FigureCanvas(self._figure)
+		return self._canvas
+
+	def _gen_cpanel (self):
+		cpanel = gtk.HBox()
+
+		sync_button = gtk.Button("Sync")
+		sync_button.connect("clicked", self._sync_action)
+		cpanel.pack_start(sync_button, True, True, 0)
+
+		shell_button = gtk.Button("Shell")
+		shell_button.connect("clicked", self._shell_action)
+		cpanel.pack_start(shell_button, True, True, 0)
+
+		quit_button = gtk.Button("Quit")
+		quit_button.connect("clicked", self._quit_action)
+		cpanel.pack_start(quit_button, True, True, 0)
+
+		return cpanel
+
+	def _sync_action (self, widget):
+		print "Sync sent!"
+
+	def _shell_action (self, widget):
+		import IPython
+		ipshell = IPython.Shell.IPythonShellEmbed()
+		ipshell()
+
+	def _quit_action (self, widget):
+		gtk.main_quit()
+
+	def get_figure (self):
+		return self._figure
+
+	def start (self):
+		gtk.main()
+
+if __name__ == "__main__":
+	x = IsiGui()
+	x.start()
+
