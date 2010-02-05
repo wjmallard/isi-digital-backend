f3852f42fd40215e4ced61fb5345f50ed6629ced
William Mallard <wjm@llard.net>
2010-02-05 05:01:19 +0000

    Forked IsiCanvas into IsiCanvas2 for future refactoring.

--- scripts/libisiplot.py
+++ scripts/libisiplot.py
@@ -99,3 +99,71 @@ class IsiCanvas (FigureCanvasGTK):
 			else:
 				self._axes_l[i].set_ylim(0, self._ymax)
 
+class IsiCanvas2 (FigureCanvasGTK):
+	def __init__ (self, figure, rows=1, cols=1):
+		FigureCanvasGTK.__init__(self, figure)
+
+		self._fig = figure
+		self._num_rows = rows
+		self._num_cols = cols
+		self._num_plots = rows * cols
+
+		self._rect_l = []
+		self._axes_l = []
+		self._contour_l = []
+		self._label_l = []
+
+		self._init_plots()
+
+	def _init_plots (self):
+		width = 1. / self._num_cols
+		height = 1. / self._num_rows
+
+		for i in xrange(self._num_rows):
+			for j in xrange(self._num_cols):
+				rect = [j*width, 1-(i+1)*height, width, height]
+				self._rect_l += [rect]
+
+		self._axes_l = [None] * self._num_plots
+		self._contour_l = [None] * self._num_plots
+		self._label_l = [None] * self._num_plots
+
+	def add_plot (self, id, data, label=None):
+		if self._axes_l[id] != None:
+			x = id / self._num_cols
+			y = id % self._num_cols
+			print "WARN: Plot (%d,%d) already exists!" % (x, y)
+			return
+
+		ydata = [1] * len(data)
+
+		axprops = dict(xticklabels=[], yticklabels=[])
+		rect = self._rect_l[id]
+		axes = self._fig.add_axes(rect, **axprops)
+		contour, = axes.semilogy(ydata, '.')
+
+		contour.set_ydata(data)
+
+		if label != None:
+			contour.set_label(label)
+			label_xpos = self._rect_l[id][0] + .01
+			label_ypos = self._rect_l[id][1]
+			self._fig.text(label_xpos, label_ypos, label, fontsize=8)
+
+		axes.set_yticklabels([], visible=False)
+		axes.set_xlim(xmin=0, xmax=len(data))
+		axes.set_ylim(ymin=min(data), ymax=max(data))
+
+		axes.xaxis.set_ticks_position("bottom")
+		axes.yaxis.set_ticks_position("left")
+
+		self._axes_l[id] = axes
+		self._contour_l[id] = contour
+		self._label_l[id] = label
+
+		return contour
+
+	def set_data (self, id, data):
+		self._contour_l[id].set_ydata(data)
+		self._axes_l[id].set_ylim(ymin=min(data), ymax=max(data))
+
