a9acdf51ae7b2c73d9ef36ee22482eae1baa98cc
William Mallard <wjm@llard.net>
2010-02-05 03:47:42 +0000

    Rewrote to use libisigui.

--- scripts/isi_correlator_tvg.py
+++ scripts/isi_correlator_tvg.py
@@ -6,30 +6,40 @@ __copyright__ = "Copyright 2010, CASPER"
 __license__ = "GPL"
 __status__ = "Development"
 
-from libisidebug import *
+from libisigui import *
+from libisicorr import *
 from libisitvg import *
 
-import IPython
-ipshell = IPython.Shell.IPythonShellEmbed()
-
 L=2**7
-s1 = xrange(L)
-d1 = interleave_tvg_bram(s1, 16, 2**11)
-tv1 = scale_bram_list(d1, coeff=1)
+f1 = sine_wave(L, phase=90)
+f2 = sine_wave(L, phase=90)
+f3 = sine_wave(L, phase=90)
+#n1 = random_noise(L, seed=0xFC96)
+#n2 = random_noise(L, seed=0xEB85)
+#n3 = random_noise(L, seed=0xDA74)
+#s1 = fxn_sum((f1,))
+#s2 = fxn_sum((f1,))
+#s3 = fxn_sum(([0]*L,))
+d1 = interleave_tvg_bram(f1, 16, 2**11)
+d2 = interleave_tvg_bram(f2, 16, 2**11)
+d3 = interleave_tvg_bram(f3, 16, 2**11)
+tv1 = scale_bram_list(d1)
+tv2 = scale_bram_list(d2)
+tv3 = scale_bram_list(d3)
 
-R = IsiCorrelatorDebug('localhost', 7147)
+hosts = ('localhost', 'localhost', 'fake')
+ports = (7147, 7148, 7149)
+I = IsiCorrelator(hosts, ports)
+D = IsiGui(I)
 
-R.load_tvg(tv1)
+#I.program('isi_correlator_tvg.bof')
+I.load_tvg((tv1, tv2, tv3))
 
-R.set_sync_period(2**12)
-R.set_fft_shift(0x7f)
-R.set_eq_coeff((2**7)<<8)
+I.set_sync_period(2**12)
+I.set_fft_shift(0x7f)
+I.set_eq_coeff((2**7)<<8)
 
-R.acquire()
-R.arm_sync()
-R.send_sync()
+I.send_sync()
 
-data = R.read_adc("adc_capt_4x", 128)
-
-ipshell()
+D.start()
 
