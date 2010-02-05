bfc31a42afe7b34488bc3b248df1bfd4cf354025
William Mallard <wjm@llard.net>
2010-02-05 03:53:16 +0000

    Removed tvg cruft.

--- scripts/isi_correlator.py
+++ scripts/isi_correlator.py
@@ -6,34 +6,15 @@ __copyright__ = "Copyright 2010, CASPER"
 __license__ = "GPL"
 __status__ = "Development"
 
-from libisigui import *
-from libisicorr import *
-from libisitvg import *
+import libisigui
+import libisicorr
 
-L=2**7
-f1 = sine_wave(L, cycles=8, phase=0)
-f2 = sine_wave(L, cycles=8, phase=.5)
-f3 = sine_wave(L, cycles=16, phase=0)
-n1 = random_noise(L, seed=0xFC96)
-n2 = random_noise(L, seed=0xEB85)
-n3 = random_noise(L, seed=0xDA74)
-s1 = fxn_sum((f1,))
-s2 = fxn_sum((f1,))
-s3 = fxn_sum(([0]*L,))
-d1 = interleave_tvg_bram(s1, 16, 2**11)
-d2 = interleave_tvg_bram(s2, 16, 2**11)
-d3 = interleave_tvg_bram(s3, 16, 2**11)
-tv1 = scale_bram_list(d1)
-tv2 = scale_bram_list(d2)
-tv3 = scale_bram_list(d3)
-
-hosts = ('localhost', 'localhost', 'localhost')
+hosts = ('localhost', 'fake', 'fake')
 ports = (7147, 7148, 7149)
-I = IsiCorrelator(hosts, ports)
-D = IsiGui(I)
+I = libisicorr.IsiCorrelator(hosts, ports)
+D = libisigui.IsiGui(I)
 
-#I.program('isi_correlator_tvg.bof')
-I.load_tvg((tv1, tv2, tv3))
+I.program('isi_correlator.bof')
 
 I.set_sync_period(2**12)
 I.set_fft_shift(0x7f)
