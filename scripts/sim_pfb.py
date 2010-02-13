da94d3cc98f9305617e5bcfba07a99faa556e60f
William Mallard <wjm@llard.net>
2010-02-13 05:01:56 +0000

    Cleaned up fft_real.

--- scripts/sim_pfb.py
+++ scripts/sim_pfb.py
@@ -2,10 +2,7 @@
 
 import pylab
 from pylab import plot
-
 import numpy as np
-from numpy.fft import fftshift
-from numpy.fft import rfft
 
 def sine_fxn (length, cycles, windows=1):
 	assert (length > 0)
@@ -20,7 +17,7 @@ def sine_fxn (length, cycles, windows=1):
 def pfb_fir (data, taps=4):
 	assert (taps>0)
 	length = len(data)
-	x = np.arange(-length/2, length/2)/(float(length)/taps)
+	x = np.arange(-length/4, length/2)/(float(length)/taps)
 	s = np.sinc(x)
 	w = np.hanning(length)
 	A = s*w*data
@@ -29,17 +26,15 @@ def pfb_fir (data, taps=4):
 	D = C.sum(axis=0)
 	return D
 
-def fft_real (data, split=1):
-	A = np.array_split(data, split)
-	B = A[0]
-	C = np.fft.rfft(B)
-	D = np.fft.fftshift(C)
-	E = np.abs(D)
-	return E
+def fft_real (data):
+	A = np.fft.rfft(data)
+	B = np.fft.fftshift(A)
+	C = np.abs(B)
+	return C
 
 adc = sine_fxn(128, np.pi, 4)
 pfb = pfb_fir(adc, taps=4)
-fft_raw = fft_real(adc, 4)
+fft_raw = fft_real(adc[0:128])
 fft_pfb = fft_real(pfb)
 
 plot(adc)
