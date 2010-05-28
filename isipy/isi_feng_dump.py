ed735f2ebf002c78b228a9b57dd222ca2777e95a
William Mallard <wjm@llard.net>
2010-05-28 21:43:17 +0000

    Created isi_feng_dump to store received data to files.

---
+++ isipy/isi_feng_dump.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+__author__ = "William Mallard"
+__email__ = "wjm@llard.net"
+__copyright__ = "Copyright 2010, CASPER"
+__license__ = "GPL"
+__status__ = "Development"
+
+import libisiroach
+import socket
+import sys
+
+import numpy as np
+from libisiroach import IsiRoachBoard
+from time import strftime
+
+DATA_SOCK = "/tmp/isi_data_sock"
+CTRL_SOCK = "/tmp/isi_ctrl_sock"
+
+datafmt = np.dtype \
+([
+	('pkt_id', '>i4'), 
+	('adc', '>i4', 512),
+	('pfb', '>i4', 128),
+	('fftR', '>i4', 64),
+	('fftI', '>i4', 64),
+	('raR', '>i4', 64),
+	('raI', '>i4', 64),
+])
+DATA = np.zeros(1, dtype=datafmt)
+
+def open_data_client_sock (sockname):
+	sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+	try:
+		sock.bind(sockname)
+	except socket.error:
+		print "WARN: %s exists! Removing." % sockname
+		socket.os.unlink(sockname)
+		sock.bind(sockname)
+	return sock
+
+def open_ctrl_client_sock (sockname):
+	sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+	try:
+		sock.connect(sockname)
+	except socket.error:
+		print "ERROR: Cannot connect to recv server."
+		sys.exit(1)
+	return sock
+
+def dump_to_file (name, data):
+	#timestamp = strftime("%Y%m%dT%H%M%S")
+	#filename = "data/%s_%s.dump" % (timestamp, name)
+	filename = "data/%s.dump" % (name)
+	f = open(filename, "w")
+	for i in data:
+		f.write("%d\n" % i)
+	f.close()
+
+if __name__ == "__main__":
+
+	data_sock = open_data_client_sock(DATA_SOCK)
+	ctrl_sock = open_ctrl_client_sock(CTRL_SOCK)
+	ctrl_sock.send("subscribe %s" % DATA_SOCK)
+	data_sock.recv_into(DATA)
+
+	dump_to_file("adc", DATA['adc'][0])
+	dump_to_file("pfb", DATA['pfb'][0])
+	dump_to_file("fftR", DATA['fftR'][0])
+	dump_to_file("fftI", DATA['fftI'][0])
+	dump_to_file("raR", DATA['raR'][0])
+	dump_to_file("raI", DATA['raI'][0])
+
+	data_sock.close()
+	ctrl_sock.close()
+
