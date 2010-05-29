a5c8531957c91ccb2def724ea91e56cfd8d5c8c4
William Mallard <wjm@llard.net>
2010-05-29 08:20:49 +0000

    Refactored socket code in isi_feng_dump.

--- isipy/isi_feng_dump.py
+++ isipy/isi_feng_dump.py
@@ -37,6 +37,7 @@ def open_data_client_sock (sockname):
 		print "WARN: %s exists! Removing." % sockname
 		socket.os.unlink(sockname)
 		sock.bind(sockname)
+	print "Opened data socket."
 	return sock
 
 def open_ctrl_client_sock (sockname):
@@ -46,7 +47,19 @@ def open_ctrl_client_sock (sockname):
 	except socket.error:
 		print "ERROR: Cannot connect to recv server."
 		sys.exit(1)
+	print "Opened ctrl socket."
 	return sock
+
+def close_data_client_sock (sock):
+	sockname = sock.getsockname()
+	sock.close()
+	socket.os.unlink(sockname)
+	print "Closed data socket."
+
+def close_ctrl_client_sock (sock):
+	sock.close()
+	print "Closed ctrl socket."
+
 
 def dump_to_file (name, data):
 	#timestamp = strftime("%Y%m%dT%H%M%S")
@@ -63,14 +76,14 @@ if __name__ == "__main__":
 	ctrl_sock = open_ctrl_client_sock(CTRL_SOCK)
 	ctrl_sock.send("subscribe %s" % DATA_SOCK)
 	data_sock.recv_into(DATA)
+	close_data_client_sock(data_sock)
+	close_ctrl_client_sock(ctrl_sock)
 
+	print "Dumping data to file ..."
 	dump_to_file("adc", DATA['adc'][0])
 	dump_to_file("pfb", DATA['pfb'][0])
 	dump_to_file("fftR", DATA['fftR'][0])
 	dump_to_file("fftI", DATA['fftI'][0])
 	dump_to_file("raR", DATA['raR'][0])
 	dump_to_file("raI", DATA['raI'][0])
-
-	data_sock.close()
-	ctrl_sock.close()
 
